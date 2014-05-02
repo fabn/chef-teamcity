c106560cdf35f8680f22689b6f3cb26b45e2553b
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-05-02 19:10:20 +0200

    Teamcity version bump

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,10 +5,10 @@ default[:teamcity][:system][:home] = '/opt/teamcity'
 default[:teamcity][:system][:generate_keys] = false
 
 # Teamcity archive and checksum
-default[:teamcity][:server][:version] = '8.1.1'
+default[:teamcity][:server][:version] = '8.1.2'
 default[:teamcity][:server][:base_url] = 'http://download-cf.jetbrains.com/teamcity'
 default[:teamcity][:server][:download_url] = "#{node[:teamcity][:server][:base_url]}/TeamCity-#{node[:teamcity][:server][:version]}.tar.gz"
-default[:teamcity][:server][:checksum] = 'edfa219b2f208ff41ceed1d79b962208e81a3c33bfbbe6781eb2bb111e4ec903' # For v 8.1.1
+default[:teamcity][:server][:checksum] = '1376ad7d59b5aa20886e04e6e1d701170c1a11bc1f0adc408fcd8ef38c8dd80e' # For v 8.1.2
 # Server installation attributes
 default[:teamcity][:server][:port] = 8111
 default[:teamcity][:server][:listen_address] = '127.0.0.1'
